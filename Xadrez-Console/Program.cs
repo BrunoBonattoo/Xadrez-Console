f34422b0d634c3f8a536579a8b2be0134f14b74d
Bruno Bonatto <brunobonatto@id.uff.br>
2023-03-31 14:32:16 -0300

    Tratando erros com execeptions

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -7,12 +7,18 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
+            try { 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
+            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(0, 0));
             Tela.ImprimirTabuleiro(tab);
+            }
+            catch(TabuleiroExeception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
