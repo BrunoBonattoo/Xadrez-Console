858d0d99e6d2964b89f0f5a99beaad32d67d5fab
Bruno Bonatto <brunobonatto@id.uff.br>
2023-03-31 10:59:10 -0300

    Começando a colocar peças no tabuleiro

--- Xadrez-Console/Tabuleiro/Peca.cs
+++ Xadrez-Console/Tabuleiro/Peca.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Runtime.ConstrainedExecution;
 using tabuleiro;
 
 namespace tabuleiro
@@ -10,9 +11,9 @@ namespace tabuleiro
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.qteMovimentos = 0;
